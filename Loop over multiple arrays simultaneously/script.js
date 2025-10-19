14ada6da3c0dd383999513f99e4654106693312b
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-10-19 15:11:26 +0200

    Implement function to loop over multiple arrays
    Added a function to concatenate elements from multiple arrays.

---
+++ Loop over multiple arrays simultaneously/script.js
@@ -0,0 +1,21 @@
+/* Loop over multiple arrays simultaneously
+Loop over multiple arrays and create a new array whose  ith
+  element is the concatenation of  ith
+  element of each of the given.
+
+For this example, if you are given this array of arrays:
+
+[ ["a", "b", "c"], ["A", "B", "C"], [1, 2, 3] ]
+the output should be:
+
+["aA1","bB2","cC3"]
+Write a function that takes an array of arrays as a parameter and returns an array of strings satisfying the given description.
+*/
+
+function loopSimult(A) {
+   let res=[];
+   for(let i=0;i<A[0].length;i++)
+   {res.push(A.map((item)=>item[i]).join(""));}
+   return res;
+}
+loopSimult([["a", "b", "c"], ["A", "B", "C"], [1, 2, 3]]);
